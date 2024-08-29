17329d439920614d5909518b2308523999219c98
dcaayushd <aayushdc123@yahoo.com>
2024-08-29 19:52:35 +0545

    Updated HomeScreen

--- mobank/lib/main.dart
+++ mobank/lib/main.dart
@@ -1,20 +1,106 @@
+import 'package:mobank/screens/activity_screen.dart';
+import 'package:mobank/screens/home_screen.dart';
+import 'package:mobank/screens/my_card_screen.dart';
+import 'package:mobank/screens/profile_screen.dart';
+import 'package:mobank/screens/scan_screen.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Mobank App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 16, 80, 98),
+        ),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    const HomeScreen(),
+    const MyCardScreen(),
+    const ScanScreen(),
+    const ActivityScreen(),
+    const ProfileScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            tabItem(Icons.home, "Home", 0),
+            tabItem(Icons.credit_card, "My Card", 1),
+            FloatingActionButton(
+              onPressed: () => onTabTapped(2),
+              backgroundColor: const Color.fromARGB(255, 16, 80, 98),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              child: const Icon(
+                Icons.qr_code_scanner,
+                color: Colors.white,
+              ),
+            ),
+            tabItem(Icons.bar_chart, "Activity", 3),
+            tabItem(Icons.person, "Profile", 4),
+          ],
         ),
       ),
     );
+  }
+
+  Widget tabItem(IconData icon, String label, int index) {
+    return IconButton(
+      onPressed: () => onTabTapped(index),
+      icon: Column(
+        children: [
+          Icon(
+            icon,
+            color: currentIndex == index ? Colors.black : Colors.grey,
+          ),
+          Text(
+            label,
+            style: TextStyle(
+                fontSize: 10,
+                color: currentIndex == index
+                    ? Theme.of(context).primaryColor
+                    : Colors.grey),
+          )
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
   }
 }
