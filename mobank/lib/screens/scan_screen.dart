47fc18d75314e5f1262a46d03244860a69da6266
dcaayushd <aayushdc123@yahoo.com>
2024-08-29 19:53:24 +0545

    Scan Screen Added

---
+++ mobank/lib/screens/scan_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ScanScreen extends StatelessWidget {
+  const ScanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Scan page"),
+      ),
+    );
+  }
+}
