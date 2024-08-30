dc988509354e3a0ac7ba71dfa9c7c33637ea4022
dcaayushd <aayushdc123@yahoo.com>
2024-08-30 20:48:31 +0545

    Splash Screen Added

--- carrentui/lib/main.dart
+++ carrentui/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carrentui/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Car Rent UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const SplashScreen(),
     );
   }
 }
