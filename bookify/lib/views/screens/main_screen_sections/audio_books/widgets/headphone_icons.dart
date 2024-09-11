8e7225a049c155e316c1f4b061944b51d5909174
dcaayushd <aayushdc123@yahoo.com>
2024-09-11 10:59:05 +0545

    Headphones Icon Added

---
+++ bookify/lib/views/screens/main_screen_sections/audio_books/widgets/headphone_icons.dart
@@ -0,0 +1,32 @@
+
+import 'package:ficonsax/ficonsax.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../theme/colors.dart';
+
+class HeadphoneIcon extends StatelessWidget {
+  const HeadphoneIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+
+    return Container(
+      height: w * .19,
+      width: w * .19,
+      margin: EdgeInsets.only(bottom: h * .015),
+      alignment: Alignment.topCenter,
+      decoration: const BoxDecoration(shape: BoxShape.circle, color: blackColor, boxShadow: [
+        BoxShadow(offset: Offset(0, 2), color: Colors.black, spreadRadius: 1, blurRadius: 3)
+      ]),
+      child: const Center(
+        child: Icon(
+          IconsaxOutline.headphone,
+          color: mainColor,
+          size: 45,
+        ),
+      ),
+    );
+  }
+}
