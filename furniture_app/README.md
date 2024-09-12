40418b0a8994373448c48d588b6ffdc0a3fcc0b9
dcaayushd <aayushdc123@yahoo.com>
2024-09-12 10:37:15 +0545

    Furniture App UI Created

---
+++ furniture_app/README.md
@@ -0,0 +1,3 @@
+# furniture_app
+
+A new Flutter project.
---
+++ furniture_app/android/app/src/main/kotlin/com/codepluscircle/furniture_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codepluscircle.furniture_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
