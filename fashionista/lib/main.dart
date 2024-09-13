3f334200ae4ff2acae43f655dcecd2ef03ccce24
dcaayushd <aayushdc123@yahoo.com>
2024-09-13 20:07:33 +0545

    Home Screen Added

--- fashionista/lib/main.dart
+++ fashionista/lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../screens/onboarding/onboarding.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Fashionista',
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const OnboardingScreen(),
     );
   }
 }
---
+++ fashionista/lib/screens/bottom_nav_bar/home/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
