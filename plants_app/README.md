b9715cf4606d43472cff8c085cede7488cdb3fa9
dcaayushd <aayushdc123@yahoo.com>
2024-09-14 19:12:11 +0545

    Plants App Initialized

---
+++ plants_app/README.md
@@ -0,0 +1,3 @@
+# plants_app
+
+A new Flutter project.
---
+++ plants_app/android/app/src/main/kotlin/com/codepluscircle/plants_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codepluscircle.plants_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
