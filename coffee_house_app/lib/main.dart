a40bf5a777c0eb17d74f42e7eebc8ad19ec531e9
dcaayushd <aayushdc123@yahoo.com>
2024-09-16 18:28:31 +0545

    Coffee House Detail Screen Added

--- coffee_house_app/lib/main.dart
+++ coffee_house_app/lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import 'package:coffee_house_app/views/screens/home/home.dart';
+
+// inspired by https://dribbble.com/shots/8245907-Coffee-House-App-Design
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.bigShouldersTextTextTheme(
+          Theme.of(context).textTheme,
         ),
       ),
+      home: const HomeScreen(),
     );
   }
 }
