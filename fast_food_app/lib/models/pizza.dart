4255c54599209b824a8ff90fbeaf06e4bb8398ca
dcaayushd <aayushdc123@yahoo.com>
2024-09-18 12:07:05 +0545

    Donut, Nutrient, Pizza & Smoothie Models Added

---
+++ fast_food_app/lib/models/pizza.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pizza {
+  String name;
+  int price;
+  Color color;
+  String image;
+
+  Pizza({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+}
