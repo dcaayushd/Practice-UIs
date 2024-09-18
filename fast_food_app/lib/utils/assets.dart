118553907541da3101d52c6fe9d20000ec1ec0ca
dcaayushd <aayushdc123@yahoo.com>
2024-09-18 12:05:29 +0545

    Fast Food App Assets Added

---
+++ fast_food_app/lib/utils/assets.dart
@@ -0,0 +1,68 @@
+class AppAssets {
+  static HomePageAssets homePageAssets = const HomePageAssets();
+  static DonutAssets donutAssets = const DonutAssets();
+  static SmoothieAssets smoothieAssets  = const SmoothieAssets();
+
+}
+
+class HomePageAssets {
+  const HomePageAssets();
+  final String homepageTabIconDonut = "assets/icons/donut.png";
+  final String homepageTabIconBurger = "assets/icons/burger.png";
+  final String homepageTabIconSmoothie =
+      "assets/icons/smoothie.png";
+  final String homepageTabIconPancake =
+      "assets/icons/pancake.png";
+  final String homepageTabIconPizza = "assets/icons/pizza.png";
+}
+
+class DonutAssets {
+  const DonutAssets();
+  final String donutChochGlaze =
+      "assets/images/donuts/choclateglaze_donut.png";
+
+  final String donutClassicSprinkle =
+      "assets/images/donuts/classicsprinkle_donut.png";
+
+  final String donutStrawberryLemon =
+      "assets/images/donuts/strawberrylemon_donut.png";
+
+  final String donutChocolateCherry =
+      "assets/images/donuts/choclatecherry_donut.png";
+
+  final String donutChocoCocoTap =
+      "assets/images/donuts/chocococotap_donut.png";
+
+  final String donutStrawberryWheel =
+      "assets/images/donuts/strawberrywheel_donut.png";
+
+  final String donutRaspberryVanilla =
+      "assets/images/donuts/raspberryvanila_donut.png";
+}
+
+class SmoothieAssets {
+  const SmoothieAssets();
+  final String smoothieSweetStrawberry =
+      "assets/images/smoothies/sweet_strawberry.png";
+
+  final String smoothieLeafyBerries =
+      "assets/images/smoothies/leafy_berries.png";
+
+  final String smoothieCleansingGreen =
+      "assets/images/smoothies/cleansing_green.png";
+
+  final String smoothieBananaBonus =
+      "assets/images/smoothies/banana_bonus.png";
+
+  final String smoothieChocoBerry =
+      "assets/images/smoothies/choco_berry.png";
+
+  final String smoothieMangoMelon =
+      "assets/images/smoothies/mango_melon.png";
+
+  final String smoothieNonStopApricot =
+      "assets/images/smoothies/nonstop_apricot.png";
+
+  final String smoothieCuteCherry =
+      "assets/images/smoothies/cute_cherry.png";
+}
