113d8e465e5ed60d34ab436f7f89f160a8e9e495
dcaayushd <aayushdc123@yahoo.com>
2024-09-21 20:01:15 +0545

    Kayak Models Added

---
+++ kayak_booking_app/lib/models/kayak.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Kayak {
+  final String name;
+  final String image;
+  final Color color;
+
+  Kayak({
+    required this.color,
+    required this.name,
+    required this.image,
+  });
+}
