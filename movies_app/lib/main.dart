2baf27b5854c6ec2f114b0ef75a5cdf2a112f552
dcaayushd <aayushdc123@yahoo.com>
2024-09-22 11:04:30 +0545

    Movie App Theme Added

--- movies_app/lib/main.dart
+++ movies_app/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../views/screens/home/home.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const HomeScreen(),
     );
   }
 }
