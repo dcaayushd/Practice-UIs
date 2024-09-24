6efc5e0e860aecfcb8dbcf9e71f634335da0f698
dcaayushd <aayushdc123@yahoo.com>
2024-09-24 17:45:57 +0545

    Recipe App Home Added

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(primarySwatch: Colors.blue, textTheme: GoogleFonts.nunitoTextTheme(ThemeData.light().textTheme)),
+      home: const HomeScreen(),
     );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
