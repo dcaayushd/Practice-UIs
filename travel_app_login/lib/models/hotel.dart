af90884a712399b0fad6c46796ebad6f25c45c90
dcaayushd <aayushdc123@yahoo.com>
2024-09-30 17:53:00 +0545

    Hotel Model Added

---
+++ travel_app_login/lib/models/hotel.dart
@@ -0,0 +1,14 @@
+class Hotel {
+  String imageUrl;
+  String name;
+  String address;
+  int price;
+
+  Hotel({
+    required this.imageUrl,
+    required this.name,
+    required this.address,
+    required this.price,
+  });
+}
+
