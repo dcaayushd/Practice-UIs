68e2feb037db1c6bfc057baeb2746f24a2c1e1bc
dcaayushd <aayushdc123@yahoo.com>
2024-10-02 15:47:40 +0545

    Whatsapp Tab View Added

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/whatsapp_tab_view.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0XFF075E54),
         ),
       ),
+      home: const WhatsAppTab(),
     );
   }
 }
