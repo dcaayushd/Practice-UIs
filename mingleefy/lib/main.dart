d546931e108897b1d7fbdb0c0c1f501b8f175b68
dcaayushd <aayushdc123@yahoo.com>
2024-10-08 15:26:23 +0545

    Landing Page Added

--- mingleefy/lib/main.dart
+++ mingleefy/lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mingleefy/pages/landing_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: LandingPage(),
     );
   }
 }
