6cfaa485fc9ee2b842839b4aa239a3f6dbe25cc8
dcaayushd <aayushdc123@yahoo.com>
2024-10-10 19:58:10 +0545

    Colors Updated

--- wellbeing_app/lib/utils/colors.dart
+++ wellbeing_app/lib/utils/colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class WellBeingColors {
-  static final darkBlueColor = Color(0xFF536490);
-  static final darkBlueEndingColor = Color(0xFF536490);
-  static final yellowColor = Color(0xFFfbb040);
-  static final veryLightMaroon = Color(0xFFfee1f3);
-  static final lightMaroon = Color(0xFFe3a4c9);
-  static final darkMaroon = Color(0xFF6d3756);
-  static final veryDarkMaroon = Color(0xFF7d4666);
+  static const darkBlueColor = Color(0xFF536490);
+  static const darkBlueEndingColor = Color(0xFF536490);
+  static const yellowColor = Color(0xFFfbb040);
+  static const veryLightMaroon = Color(0xFFfee1f3);
+  static const lightMaroon = Color(0xFFe3a4c9);
+  static const darkMaroon = Color(0xFF6d3756);
+  static const veryDarkMaroon = Color(0xFF7d4666);
   static const darkBlueGrey = Color(0xFF4e4f61);
   static const lightGrey = Color(0xFFdedede);
   static final mediumGrey = Colors.grey[800]!;
