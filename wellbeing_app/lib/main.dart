9b26ed7d39bf07253b76c186cf339f3a1f81b839
dcaayushd <aayushdc123@yahoo.com>
2024-10-10 20:00:11 +0545

    Launch Page Added

--- wellbeing_app/lib/main.dart
+++ wellbeing_app/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wellbeing_app/pages/launch_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(),
       ),
+      home: const LaunchPage(),
     );
   }
 }
