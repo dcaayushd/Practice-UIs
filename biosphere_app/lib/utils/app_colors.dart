bc64831234b5c0c27cbf8c4625f7b8df0927a976
dcaayushd <aayushdc123@yahoo.com>
2024-10-16 17:27:13 +0545

    Biosphere App Colors Added

---
+++ biosphere_app/lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBlue = Color(0xFFb7ccde);
+  static const Color lightGreen = Color(0xFFd6dfdf);
+  static const Color lightPink = Color(0xFFf7d1d2);
+}
