4e6928092dd2bd3189ecb0398394b3e7f4054a55
dcaayushd <aayushdc123@yahoo.com>
2024-10-16 17:28:01 +0545

    Landing Page Added

--- biosphere_app/lib/main.dart
+++ biosphere_app/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:biosphere_app/pages/landing_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LandingPage(),
     );
   }
 }
