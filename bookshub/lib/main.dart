7675db8dbaa8605135065408b3be3cbfadb9dcfb
dcaayushd <aayushdc123@yahoo.com>
2024-10-17 18:36:33 +0545

    BooksHub HomePage Added

--- bookshub/lib/main.dart
+++ bookshub/lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:bookshub/pages/home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'BooksHub',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.gentiumBookPlusTextTheme(),
       ),
+      home: const HomePage(),
     );
   }
 }
