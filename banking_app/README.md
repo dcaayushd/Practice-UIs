145f8c5e6d0b666a71c5a4d869246fe928b6e3e1
dcaayushd <aayushdc123@yahoo.com>
2024-10-21 14:25:07 +0545

    Banking App Created

---
+++ banking_app/README.md
@@ -0,0 +1,3 @@
+# banking_app
+
+A new Flutter project.
---
+++ banking_app/android/app/src/main/kotlin/com/codepluscircle/banking_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codepluscircle.banking_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
