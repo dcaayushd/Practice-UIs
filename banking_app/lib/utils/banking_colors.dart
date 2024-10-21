028d3deb8782545727ac97e40a8449e02094891f
dcaayushd <aayushdc123@yahoo.com>
2024-10-21 14:47:01 +0545

    Banking App Colors Added

---
+++ banking_app/lib/utils/banking_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BankingColors {
+  static const veryLightGreen = Color.fromARGB(255, 226, 246, 246);
+  static const lightGreen = Color(0xFF65b8b1);
+  static const darkGreen = Color(0xFF56b2aa);
+  static const plantGreen = Color(0xFF27ad60);
+  static const veryLightRed = Color.fromARGB(255, 254, 232, 224);
+  static const lightRed = Color(0xFFcc5a4e);
+  static const darkRed = Color(0xFFc33c29);
+  static const lightBlueGrey = Color(0xFFc33c29);
+  static const darkBlueGrey = Color(0xFF494969);
+  static const veryLightGrey = Color.fromARGB(255, 237, 237, 237);
+  static const lightGrey = Color(0xFFdfdfdf);
+  static const yellow = Color(0xFFe5be90);
+}
