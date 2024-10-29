dfbd516e6bb6971fae8898b197f13ef25e3ca223
dcaayushd <aayushdc123@yahoo.com>
2024-10-29 21:50:17 +0545

    Profile Page Added

--- tesla_app/lib/main.dart
+++ tesla_app/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tesla_app/pages/profile_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return const MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      home: ProfilePage(),
     );
   }
 }
