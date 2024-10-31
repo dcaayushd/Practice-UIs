de97f747e11d009019d18198b07717757a1531da
dcaayushd <aayushdc123@yahoo.com>
2024-10-31 18:51:51 +0545

    Colors Updated

--- ford_app/lib/utilities/colors.dart
+++ ford_app/lib/utilities/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FordColors {
-  static final grey = Color.fromRGBO(208, 208, 208, 1);
-  static final lightGrey = Color.fromRGBO(219, 219, 219, 1);
-  static final blue = Color.fromRGBO(83, 191, 204, 1);
+  static const grey = Color.fromRGBO(208, 208, 208, 1);
+  static const lightGrey = Color.fromRGBO(219, 219, 219, 1);
+  static const blue = Color.fromRGBO(83, 191, 204, 1);
 }
