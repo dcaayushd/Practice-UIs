3ecee29be008df5ee5f5eb035e4713b6a36fd2e2
dcaayushd <aayushdc123@yahoo.com>
2024-11-03 08:55:05 +0545

    Landing Page Added

--- bankify/lib/main.dart
+++ bankify/lib/main.dart
@@ -1,20 +1,18 @@
+import 'package:bankify/landing_page/landing_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LandingPage(),
     );
   }
 }
