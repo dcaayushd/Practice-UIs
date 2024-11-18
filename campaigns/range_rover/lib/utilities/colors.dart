fbd3f6b740d4ea3440f071a5ce25108749d60b29
dcaayushd <aayushdc123@yahoo.com>
2024-11-18 21:04:17 +0545

    Range Rover Colors Added

---
+++ campaigns/range_rover/lib/utilities/colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RangeRoverColors {
+  static const darkGreenColor = Color(0xFF062426);
+  static const darkBlueColor = Color(0xFF33535a);
+  static const yellowColor = Color(0xFFffd952);
+  static const greyColor = Color(0xFF979797);
+  static const lightGreenColor = Color(0xFF00fcce);
+  static const greenColor = Color(0xFF10a476);
+  static const darkGreyColor = Color(0xFF436772);
+  static const darkGreyColor2 = Color(0xFF3e5657);
+  static const darkGreyColor3 = Color(0xFF1b3135);
+
+  static const greyBackgroundColorDark = Color.fromRGBO(13, 12, 17, 1);
+  static const greyBackgroundColorLight = Color.fromRGBO(65, 65, 66, 1);
+  static const greyIconBackgroundColor = Color.fromRGBO(67, 67, 67, 1);
+}
