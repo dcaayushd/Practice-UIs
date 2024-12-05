dafbd3501019ed0f7b38ed4b48ceeaecfd6bb9c4
dcaayushd <aayushdc123@yahoo.com>
2024-12-05 17:08:56 +0545

    Chat Listing Page Added

--- social_chats/lib/main.dart
+++ social_chats/lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:social_chats/pages/chat_listing_page.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatListingPage(),
+    ),
+  );
 }
