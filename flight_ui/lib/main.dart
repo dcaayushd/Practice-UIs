980e7e1b90ab5049ff6dfa41f5867e6d98464570
dcaayushd <aayushdc123@yahoo.com>
2024-12-19 18:09:00 +0545

    Flight UI Home Screen Added

--- flight_ui/lib/main.dart
+++ flight_ui/lib/main.dart
@@ -1,20 +1,17 @@
+import 'package:flight_ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
     );
   }
 }
